f65567ca3ce3f489b1b2e8e56f55260ffed196fe
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-12-18 20:57:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,6 +43,16 @@ class Student
     DB[:conn].execute(sql)
   end
   
+   def self.all_students_in_grade_9
+    sql = <<-SQL
+     SELECT *
+     FROM students
+     WHERE grade < 12
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
