dffe70b2c8066e1ac30e6c9032b3b9d10082a57c
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-12-18 20:59:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,9 @@ class Student
      WHERE grade < 12
     SQL
     
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
   end
   
   def save
