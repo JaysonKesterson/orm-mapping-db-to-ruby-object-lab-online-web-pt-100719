c94973e88f0acb33baf5d56469995619fa03df3a
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-12-18 21:03:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,17 +56,16 @@ class Student
   end
   
   def self.first_X_students_in_grade_10(x)
-    number = x
     sql = <<-SQL
      SELECT *
      FROM students
      WHERE grade = 10
-     LIMIT number
     SQL
     
-    DB[:conn].execute(sql).collect do |row|
+    x.times do DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
     end
+   end
   end
   
   def save
